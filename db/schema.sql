0bd514f3e81d1531060fe233cf64b7c9f26f6a18
dan <chronos937@gmail.com>
2019-12-07 17:15:52 -0500

    created file/directory structure

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+drop database if exists burgers_db;
+create database burgers_db;
+use burgers_db;
+
+create table burgers_db
+(
+    id int auto_increment not null,
+    burger_name varchar (30),
+    devoured boolean,
+    primary key (id)
+)
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+insert into burgers (burger_name, devoured)
+values ("Mushroom Bacon Swiss", false),
+("Double Bacon Cheddar", false),
+("Impossible Burger", false);
