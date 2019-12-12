af6c117bbc0853c97fb5d44a0be996a534a177d5
dan <chronos937@gmail.com>
2019-12-12 18:06:55 -0500

    added base function of router update

--- controllers/burgers_contorller.js
+++ controllers/burgers_contorller.js
@@ -2,18 +2,33 @@ var express = require("express");
 var burgers = require("../models/burgers.js");
 var router = express.Router();
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-router.get("/burgers", function(req, res) {
-    burgers.selectAll(function(data) {
-        res.json({burgers: data});
+router.get("/burgers", function (req, res) {
+    burgers.selectAll(function (data) {
+        res.json({ burgers: data });
     });
 });
 
 router.post("/burgers", function (req, res) {
     burgers.createOne([
         ""
-    ])
-})
+    ]);
+});
+
+router.put("/burger/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    burgers.updateOne({
+        devoured: true
+    }, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        }
+        else {
+            res.json({ id: req.params.id });
+        }
+    });
+});
