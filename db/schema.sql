99a68c6c2444f1712f591b020cf9351070f5052f
dan demeo <chronos937@gmail.com>
2019-12-14 20:52:29 -0500

    corrected typo in schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ drop database if exists burgers_db;
 create database burgers_db;
 use burgers_db;
 
-create table burgers_db
+create table burgers
 (
     id int auto_increment not null,
     burger_name varchar (30),
